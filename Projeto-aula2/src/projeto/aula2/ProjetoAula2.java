f1afc48e10d3a5a798718842ad57cf2f8fbb7f2f
Felipealves0709 <felipeemy10@gmail.com>
2024-03-01 20:41:51 -0300

    Projeto aula 2 sexta

---
+++ Projeto-aula2/src/projeto/aula2/ProjetoAula2.java
@@ -0,0 +1,69 @@
+package projeto.aula2;
+
+import javax.swing.JOptionPane;
+
+import java.util.Scanner;
+
+public class ProjetoAula2 {
+
+   
+    public static void main(String[] args) {
+        //System.out.println("Hello World!"); //boa noite joao
+       // JOptionPane.showMessageDialog(null, "Hello world");
+        //JOptionPane.showMessageDialog(null, "BOA NOITE");
+        
+      /* String nome;
+       int ano_nasc , ano_Atual;
+        
+       
+       nome = JOptionPane.showInputDialog("Digite o seu nome:");
+       ano_nasc = Integer.parseInt(JOptionPane.showInputDialog("Digite seu ano de nascimento:"));
+       ano_Atual = Integer.parseInt(JOptionPane.showInputDialog("Digite o ano atual:"));
+       
+       
+       
+       
+       
+       System.out.println("O nome digitado foi: " + nome + " Tem apenas " + ( ano_Atual - ano_nasc) + " De idade");*/
+       
+       
+       
+
+       
+
+        String nome;
+        int Val_Sal;
+
+
+
+
+
+
+
+
+//nome = "PEDROCA";
+        //idade = 27;
+        //JOptionPane.showMessageDialog(null, "olá "  + nome + " A sua idade é" + idade);
+        
+        //Scanner dados = new Scanner(System.in);
+        
+   
+        //System.out.println("Digite o nome:");
+        //nome = dados.nextLine();
+        
+        //System.out.println("Digite a idade:");
+        //idade = dados.nextInt();
+        
+        //System.out.println("O nome digitado foi: "+ nome);
+       //System.out.println("A idade digitada foi: "+ idade);
+        
+       //c
+        //idade = Integer.parseInt(JOptionPane.showInputDialog("Digite sua idade:"));
+    
+        //JOptionPane.showMessage
+        
+        
+    }
+    
+    
+}
